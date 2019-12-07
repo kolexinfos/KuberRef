5efef1ccc8e8ea4a93a45d218f3fe34a0069a34a
Kehinde Kolade <kolexinfos@gmail.com>
2019-12-07 20:43:15 +0100

    Added Ingress Implementation

--- base64encode.ps1
+++ base64encode.ps1
@@ -1,4 +1,4 @@
-$value = ""
+$value = "kolexinfos"
 $valueBytes = [System.Text.Encoding]::UTF8.GetBytes($value)
 $valueBase64 = [Convert]::ToBase64String($valueBytes)
 Write-Host "Account Name Base 64: " $valueBase64
